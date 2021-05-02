ec2182b9f4aa09e28e1e08f45e4ac134548f4ea1
Patrick <tricksap@gmail.com>
2021-05-02 13:20:42 +0800

    Add backend to new patron

--- includes/new-author.php
+++ includes/new-author.php
@@ -1,15 +1,14 @@
 <?php
 include_once "db.php";
 
-$Author_ID = $_POST["Author_ID"];
 $Author_FirstName = $_POST["Author_FirstName"];
 $Author_MiddleName = $_POST["Author_MiddleName"];
 $Author_LastName = $_POST["Author_LastName"];
 $Author_StateAddress = $_POST["Author_StateAddress"];
 $Author_CountryAddress = $_POST["Author_CountryAddress"];
 
-$sql = "INSERT INTO author( Author_ID , Author_FirstName , Author_MiddleName , Author_LastName , Author_StateAddress , Author_CountryAddress )
-VALUES ('$Author_ID','$Author_FirstName','$Author_MiddleName','$Author_LastName','$Author_StateAddress','$Author_CountryAddress')";
+$sql = "INSERT INTO author(Author_FirstName , Author_MiddleName , Author_LastName , Author_StateAddress , Author_CountryAddress )
+VALUES ('$Author_FirstName','$Author_MiddleName','$Author_LastName','$Author_StateAddress','$Author_CountryAddress')";
 
 $result = mysqli_query($conn, $sql);
 
