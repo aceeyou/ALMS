ea2dd2bf7ebe5219b7e0f15f16db8b996398110b
Patrick <tricksap@gmail.com>
2021-05-04 11:03:04 +0800

    Implement delete feature fix #25

--- includes/manage-authors(backend).php
+++ includes/manage-authors(backend).php
@@ -7,20 +7,18 @@ $authors = mysqli_query($conn, $query);
 
 
 //delete author
-if (isset($_GET['delete'])){
-	$author_ID = $_GET['delete'];
+if (isset($_GET['delete'])) {
+	$author_ID = $_GET['confirmDelete'];
+	echo $author_ID;
 	$sql = "DELETE FROM AUTHOR WHERE Author_ID = $author_ID";
 	$delete = mysqli_query($conn, $sql);
-		if($delete)
-		{
-    		mysqli_close($conn); 
-    		header("location:../manage-authors.php");
-    		exit;	
-		}
-		else
-		{
-    		echo "Error deleting record";
-		}
+	if ($delete) {
+		mysqli_close($conn);
+		header("location:../manage-authors.php");
+		exit;
+	} else {
+		echo "Error deleting record";
+	}
 }
 
 //edit author
