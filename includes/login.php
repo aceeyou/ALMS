5e026d3df4bd4ba2ac3ce832381106b3b3148439
Patrick <tricksap@gmail.com>
2021-05-28 19:56:14 +0800

    Implement Session

--- includes/login.php
+++ includes/login.php
@@ -1,22 +1,28 @@
 <?php
 include_once "db.php";
 
-$userName = $_POST["username"];
-$pass = $_POST["password"];
 
-if (!empty($userName) && !empty($pass)) {
-    // read from database
-    $query  = "select * from librarian_account where Username = '$userName' limit 1";
-    $result = mysqli_query($conn, $query);
-    // check if there is a result
-    if ($result && mysqli_num_rows($result) > 0) {
-        // convert the data to array
-        $user_data = mysqli_fetch_assoc($result);
-        // check if password is correct
-        if ($user_data['Librarian_Password'] == $pass) {
-            // redirect to dashboard 
-            header("location:../dashboard.php");
-            die;
+if (isset($_POST["login"])) {
+    session_start();
+    $userName = $_POST["username"];
+    $pass = $_POST["password"];
+
+    if (!empty($userName) && !empty($pass)) {
+        // read from database
+        $query  = "select * from librarian_account where Username = '$userName' limit 1";
+        $result = mysqli_query($conn, $query);
+        // check if there is a result
+        if ($result && mysqli_num_rows($result) > 0) {
+            // convert the data to array
+            $user_data = mysqli_fetch_assoc($result);
+            // check if password is correct
+            if ($user_data['Librarian_Password'] == $pass) {
+                $_SESSION['ID'] = $user_data['LibrarianAccount_ID'];
+
+                // redirect to dashboard 
+                header("location:../dashboard.php");
+                die;
+            }
         }
     }
 }
